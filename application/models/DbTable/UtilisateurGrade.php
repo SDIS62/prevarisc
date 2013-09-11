81a7a7661ad5ea560a2e865cd98809cc54d21539
Kévin DUBUC <kevindubuc62@gmail.com>
2013-09-11 16:30:55 +0200

    Nettoyage des fichiers inutilisés

--- application/models/DbTable/UtilisateurGrade.php
+++
@@ -1,6 +0,0 @@
-<?php
-    class Model_DbTable_UtilisateurGrade extends Zend_Db_Table_Abstract
-    {
-        protected $_name="utilisateurgrade";
-        protected $_primary = array("ID_UTILISATEUR", "ID_GRADE");
-    }
