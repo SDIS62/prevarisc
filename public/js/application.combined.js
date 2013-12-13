44d4145e07d0d2a9800a59458fb0918e7a7fa068
Kévin DUBUC <kevindubuc62@gmail.com>
2013-12-13 20:03:15 +0100

    Mise à jour des plugins
    * A faire : Nettoyer complètement les plugins inutiles ou obsolètes

--- public/js/application.combined.js
+++ public/js/application.combined.js
@@ -1,6 +1,7 @@
 /* Plugins jQuery
 -------------------------------------------------- */
-<!--#include file="./jquery.min.js" -->
+<!--#include file="./jquery-1.10.2.min.js" -->
+<!--#include file="./jquery-migrate-1.2.1.min.js" -->
 <!--#include file="./jquery-ui.min.js" -->
 <!--#include file="./jquery.fullcalendar.js" -->
 <!--#include file="./jquery.autocomplete.min.js" -->
@@ -12,3 +13,11 @@
 <!--#include file="./jquery.tablesorter.pager.js" -->
 <!--#include file="./jquery.tipsy.js" -->
 <!--#include file="./jquery.fancybox-1.3.4.js" -->
+<!--#include file="./bootstrap.min.js" -->
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Titres
+    $('a[title]').tipsy({live: true});
+    $('abbr[title]').tipsy({live: true});
+    
+}, false);
--- public/js/dossier/calendrier.js
+++ public/js/dossier/calendrier.js
@@ -1,8 +1,4 @@
-
-
-
-
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function() {
 
 
 	( function($) {
