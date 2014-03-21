b7e2ba95ccf295d5f1e53df9a145daf3752ea86b
Kévin DUBUC <kevindubuc62@gmail.com>
2014-03-21 15:17:24 +0100

    Nouveau controller d'entrée

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -4,38 +4,14 @@ class IndexController extends Zend_Controller_Action
 {
     public function indexAction()
     {
-        $this->_helper->layout->setLayout('menu_left');
-
         $service_feed = new Service_Feed;
         $service_user = new Service_User;
 
-        $this->view->flux = $service_feed->get(Zend_Auth::getInstance()->getIdentity()->ID_GROUPE);
-        $this->view->groupes = $service_user->getAllGroupes();
-    }
+        $this->view->user = $service_user->find(Zend_Auth::getInstance()->getIdentity()['ID_UTILISATEUR']);
+        $this->view->flux = $service_feed->get(Zend_Auth::getInstance()->getIdentity()['group']['ID_GROUPE']);
 
-    public function addMessageAction()
-    {
-        try {
-            $service_feed = new Service_Feed;
-            $service_feed->addMessage($this->_request->getParam('type'), $this->_request->getParam('text'), $this->_request->getParam('conf') );
-            $this->_helper->flashMessenger(array('context' => 'success','title' => 'Message ajouté !','message' => 'Le message a bien été ajouté.'));
-        } catch (Exception $e) {
-            $this->_helper->flashMessenger(array('context' => 'error','title' => 'Erreur !','message' => 'Erreur lors de l\'ajout du message : ' . $e->getMessage()));
-        }
-
-        $this->_helper->redirector('index');
-    }
-
-    public function deleteMessageAction()
-    {
-        try {
-            $service_feed = new Service_Feed;
-            $service_feed->deleteMessage($this->_request->getParam('id'));
-            $this->_helper->flashMessenger(array('context' => 'success','title' => 'Message supprimé !','message' => 'Le message a bien été supprimé.'));
-        } catch (Exception $e) {
-            $this->_helper->flashMessenger(array('context' => 'error','title' => 'Erreur !','message' => 'Erreur lors de la suppression du message : ' . $e->getMessage()));
-        }
-
-        $this->_helper->redirector('index');
+        $paginator = Zend_Paginator::factory($service_user->getEtablissements(Zend_Auth::getInstance()->getIdentity()['ID_UTILISATEUR']));
+        $paginator->setItemCountPerPage(10)->setCurrentPageNumber(array_key_exists('page', $_GET) ? (int) $_GET['page'] : 1)->setDefaultScrollingStyle('Elastic');
+        $this->view->etablissements = $paginator;
     }
 }
