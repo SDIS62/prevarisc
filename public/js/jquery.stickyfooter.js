b1b55f94a69a7bcc007ace6ad158b7c31f9e2737
Kévin DUBUC <kevindubuc62@gmail.com>
2014-04-02 17:35:33 +0200

    sticky footer ! o/

---
+++ public/js/jquery.stickyfooter.js
@@ -0,0 +1,21 @@
+function stickyFooter() {
+	var footer = $("footer");
+
+	if (footer.attr('style')) {
+		footer.removeAttr('style');
+	}	
+
+	var pos = footer.position();
+	var height = $(window).height();
+	height = height - pos.top;
+	height = height - footer.outerHeight();
+
+	if (height > 0) {
+		footer.css({'margin-top' : height+'px'});
+		footer.css({'margin-bottom' : 0});
+	}
+}
+
+$(window).bind("load", stickyFooter);
+$(window).bind("resize", stickyFooter);
+$(window).bind("DOMSubtreeModified", stickyFooter);
