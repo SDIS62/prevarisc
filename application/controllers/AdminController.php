ee61e6ccbcf2d8c1789a7a6885b7f7ecc6e7c565
Kévin DUBUC <kevindubuc62@gmail.com>
2014-04-10 12:39:32 +0200

    Ajout d'informations sur la page admin

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -5,5 +5,10 @@ class AdminController extends Zend_Controller_Action
     public function indexAction()
     {
         $this->_helper->layout->setLayout('menu_admin');
+
+        $this->view->key_ign = Zend_Controller_Front::getInstance()->getParam('bootstrap')->getOption('ign')['key'];
+        $this->view->ldap_enabled = Zend_Controller_Front::getInstance()->getParam('bootstrap')->getOption('ldap')['enabled'];
+        $this->view->dbname = Zend_Controller_Front::getInstance()->getParam('bootstrap')->getOption('resources')['db']['params']['dbname'];
+        $this->view->db_url = Zend_Controller_Front::getInstance()->getParam('bootstrap')->getOption('resources')['db']['params']['host'];
     }
 }
