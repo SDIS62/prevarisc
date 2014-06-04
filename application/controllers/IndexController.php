00ac0da29600b6be335c9e8d9f7a173775658611
Kévin DUBUC <kevindubuc62@gmail.com>
2014-06-04 17:07:39 +0200

    Nouveau dashboard

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -4,14 +4,29 @@ class IndexController extends Zend_Controller_Action
 {
     public function indexAction()
     {
+        $this->_helper->layout->setLayout('dashboard');
+
+        $this->view->inlineScript()->appendFile("http://packery.metafizzy.co/packery.pkgd.min.js");
+        //$this->view->headLink()->appendStylesheet('http://ksylvest.github.io/jquery-gridly/stylesheets/jquery.gridly.css', 'all');
+
         $service_feed = new Service_Feed;
         $service_user = new Service_User;
 
         $this->view->user = $service_user->find(Zend_Auth::getInstance()->getIdentity()['ID_UTILISATEUR']);
         $this->view->flux = $service_feed->get(Zend_Auth::getInstance()->getIdentity()['group']['ID_GROUPE']);
 
-        $paginator = Zend_Paginator::factory($service_user->getEtablissements(Zend_Auth::getInstance()->getIdentity()['ID_UTILISATEUR']));
-        $paginator->setItemCountPerPage(10)->setCurrentPageNumber(array_key_exists('page', $_GET) ? (int) $_GET['page'] : 1)->setDefaultScrollingStyle('Elastic');
-        $this->view->etablissements = $paginator;
+        $data = $service_user->getDashboardData(Zend_Auth::getInstance()->getIdentity()['ID_UTILISATEUR']);
+
+        $etablissements = Zend_Paginator::factory($data['etablissements']);
+        $etablissements->setItemCountPerPage(10)->setCurrentPageNumber(array_key_exists('page', $_GET) ? (int) $_GET['page'] : 1)->setDefaultScrollingStyle('Elastic');
+        $this->view->etablissements = $etablissements;
+
+        $dossiers = Zend_Paginator::factory($data['dossiers']);
+        $dossiers->setItemCountPerPage(10)->setCurrentPageNumber(array_key_exists('page', $_GET) ? (int) $_GET['page'] : 1)->setDefaultScrollingStyle('Elastic');
+        $this->view->dossiers = $dossiers;
+
+        $commissions = Zend_Paginator::factory($data['commissions']);
+        $commissions->setItemCountPerPage(10)->setCurrentPageNumber(array_key_exists('page', $_GET) ? (int) $_GET['page'] : 1)->setDefaultScrollingStyle('Elastic');
+        $this->view->commissions = $commissions;
     }
 }
--- application/views/helpers/Avatar.php
+++ application/views/helpers/Avatar.php
@@ -1,10 +1,17 @@
 <?php
 
-    class View_Helper_Avatar extends Zend_Controller_Action_Helper_Abstract
+    class View_Helper_Avatar extends Zend_View_Helper_HtmlElement
     {
-        public function avatar($id, $size="small")
+        public function avatar($id, $size="small", $attribs = null)
         {
+            // Attributs
+            if ($attribs) {
+                $attribs = $this->_htmlAttribs($attribs);
+            } else {
+                $attribs = '';
+            }
+
             $src = DATA_PATH . "/uploads/avatars/$size/";
-            echo "<img src='" . $src . ( file_exists($_SERVER['DOCUMENT_ROOT'].$src.$id.".jpg") ? $id : "default" ) . ".jpg' alt='Avatar' />";
+            echo "<img $attribs src='" . $src . ( file_exists($_SERVER['DOCUMENT_ROOT'].$src.$id.".jpg") ? $id : "default" ) . ".jpg' alt='Avatar' />";
         }
     }
