b9451cacacf9e5e5f0900c78ada7b30d54f09d7f
Kévin DUBUC <kevindubuc62@gmail.com>
2014-11-03 15:08:01 +0100

    Fusion des controllers index et news

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -7,13 +7,13 @@ class IndexController extends Zend_Controller_Action
         $this->_helper->layout->setLayout('index');
 
         $this->view->inlineScript()->appendFile("/js/jquery.packery.pkgd.min.js");
-        
+
         $service_feed = new Service_Feed;
         $service_user = new Service_User;
 
         $this->view->user = $service_user->find(Zend_Auth::getInstance()->getIdentity()['ID_UTILISATEUR']);
         $this->view->flux = $service_feed->get(Zend_Auth::getInstance()->getIdentity()['group']['ID_GROUPE']);
-  
+
         $data = $service_user->getDashboardData(Zend_Auth::getInstance()->getIdentity()['ID_UTILISATEUR']);
 
         $etablissements = Zend_Paginator::factory($data['etablissements']);
@@ -27,11 +27,11 @@ class IndexController extends Zend_Controller_Action
         $commissions = Zend_Paginator::factory($data['commissions']);
         $commissions->setItemCountPerPage(10)->setCurrentPageNumber(array_key_exists('page', $_GET) ? (int) $_GET['page'] : 1)->setDefaultScrollingStyle('Elastic');
         $this->view->commissions = $commissions;
-        
+
         $erpSansPreventionniste = Zend_Paginator::factory($data['erpSansPreventionniste']);
         $erpSansPreventionniste->setItemCountPerPage(10)->setCurrentPageNumber(array_key_exists('page', $_GET) ? (int) $_GET['page'] : 1)->setDefaultScrollingStyle('Elastic');
         $this->view->erpSansPreventionniste =  $erpSansPreventionniste;
-        
+
         $etablissementAvisDefavorable = Zend_Paginator::factory($data['etablissementAvisDefavorable']);
         $etablissementAvisDefavorable->setItemCountPerPage(10)->setCurrentPageNumber(array_key_exists('page', $_GET) ? (int) $_GET['page'] : 1)->setDefaultScrollingStyle('Elastic');
         $this->view->etablissementAvisDefavorable =  $etablissementAvisDefavorable;
@@ -39,22 +39,52 @@ class IndexController extends Zend_Controller_Action
         $listDossierCommissionEchu = Zend_Paginator::factory($data['dossierCommissionEchu']);
         $listDossierCommissionEchu->setItemCountPerPage(10)->setCurrentPageNumber(array_key_exists('page', $_GET) ? (int) $_GET['page'] : 1)->setDefaultScrollingStyle('Elastic');
         $this->view->dossierCommissionEchu =  $listDossierCommissionEchu;
-        
+
         $listeDesCourrierSansReponse= Zend_Paginator::factory($data['CourrierSansReponse']);
         $listeDesCourrierSansReponse->setItemCountPerPage(10)->setCurrentPageNumber(array_key_exists('page', $_GET) ? (int) $_GET['page'] : 1)->setDefaultScrollingStyle('Elastic');
         $this->view->CourrierSansReponse =  $listeDesCourrierSansReponse;
-        
+
         $prochainesCommission= Zend_Paginator::factory($data['prochainesCommission']);
         $prochainesCommission->setItemCountPerPage(10)->setCurrentPageNumber(array_key_exists('page', $_GET) ? (int) $_GET['page'] : 1)->setDefaultScrollingStyle('Elastic');
         $this->view->prochainesCommission =  $prochainesCommission;
-        
+
         $NbrDossiersAffect= Zend_Paginator::factory($data['NbrDossiersAffect']);
         $NbrDossiersAffect->setItemCountPerPage(10)->setCurrentPageNumber(array_key_exists('page', $_GET) ? (int) $_GET['page'] : 1)->setDefaultScrollingStyle('Elastic');
         $this->view->NbrDossiersAffect =  $NbrDossiersAffect;
-        
+
         $ErpSansProchaineVisitePeriodeOuvert= Zend_Paginator::factory($data['ErpSansProchaineVisitePeriodeOuvert']);
         $ErpSansProchaineVisitePeriodeOuvert->setItemCountPerPage(10)->setCurrentPageNumber(array_key_exists('page', $_GET) ? (int) $_GET['page'] : 1)->setDefaultScrollingStyle('Elastic');
         $this->view->ErpSansProchaineVisitePeriodeOuvert =  $ErpSansProchaineVisitePeriodeOuvert;
-        
+
+    }
+
+    public function addMessageAction()
+    {
+        $service_feed = new Service_Feed;
+        $service_user = new Service_User;
+
+        $this->view->groupes = $service_user->getAllGroupes();
+        if ($this->_request->isPost()) {
+            try {
+                $service_feed->addMessage($this->_request->getParam('type'), $this->_request->getParam('text'), Zend_Auth::getInstance()->getIdentity()['ID_UTILISATEUR'], $this->_request->getParam('conf') );
+                $this->_helper->flashMessenger(array('context' => 'success','title' => 'Message ajouté !','message' => 'Le message a bien été ajouté.'));
+            } catch (Exception $e) {
+                $this->_helper->flashMessenger(array('context' => 'danger','title' => 'Erreur !','message' => 'Erreur lors de l\'ajout du message : ' . $e->getMessage()));
+            }
+            $this->_helper->redirector('index', 'index');
+        }
+    }
+
+    public function deleteMessageAction()
+    {
+        $service_feed = new Service_Feed;
+
+        try {
+            $service_feed->deleteMessage($this->_request->getParam('id'));
+            $this->_helper->flashMessenger(array('context' => 'success','title' => 'Message supprimé !','message' => 'Le message a bien été supprimé.'));
+        } catch (Exception $e) {
+            $this->_helper->flashMessenger(array('context' => 'danger','title' => 'Erreur !','message' => 'Erreur lors de la suppression du message : ' . $e->getMessage()));
+        }
+        $this->_helper->redirector('index', 'index');
     }
 }
