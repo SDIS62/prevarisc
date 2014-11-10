37c128228bb974e7b22c6ca278f2b202657d3f4e
Kévin DUBUC <kevindubuc62@gmail.com>
2014-11-10 10:16:20 +0100

    Recherche par type d'activité (motif de l'api)
    Aussi, la recherche se fait sur les établissements ouverts par défaut.

--- application/controllers/SearchController.php
+++ application/controllers/SearchController.php
@@ -17,7 +17,7 @@ class SearchController extends Zend_Controller_Action
         $service_statut = new Service_Statut;
         $service_avis = new Service_Avis;
         $service_categorie = new Service_Categorie;
-        $service_type = new Service_Type;
+        $service_typeactivite = new Service_TypeActivite;
         $service_famille = new Service_Famille;
         $service_classe = new Service_Classe;
 
@@ -26,7 +26,7 @@ class SearchController extends Zend_Controller_Action
         $this->view->DB_statut = $service_statut->getAll();
         $this->view->DB_avis = $service_avis->getAll();
         $this->view->DB_categorie = $service_categorie->getAll();
-        $this->view->DB_type = $service_type->getAll();
+        $this->view->DB_typeactivite = $service_typeactivite->getAllWithTypes();
         $this->view->DB_famille = $service_famille->getAll();
 
 
@@ -39,14 +39,14 @@ class SearchController extends Zend_Controller_Action
                 $categories = array_key_exists('categories', $parameters) ? $parameters['categories'] : null;
                 $classes = array_key_exists('classes', $parameters) ? $parameters['classes'] : null;
                 $familles = array_key_exists('familles', $parameters) ? $parameters['familles'] : null;
-                $types = array_key_exists('types', $parameters) ? $parameters['types'] : null;
+                $types_activites = array_key_exists('types_activites', $parameters) ? $parameters['types_activites'] : null;
                 $avis_favorable = array_key_exists('avis', $parameters) && count($parameters['avis']) == 1 ? $parameters['avis'][0] == 'true' : null;
                 $statuts = array_key_exists('statuts', $parameters) ? $parameters['statuts'] : null;
                 $local_sommeil = array_key_exists('presences_local_sommeil', $parameters) && count($parameters['presences_local_sommeil']) == 1 ? $parameters['presences_local_sommeil'][0] == 'true' : null;
                 $city = array_key_exists('city', $parameters) && $parameters['city'] != '' ? $parameters['city'] : null;
                 $street = array_key_exists('street', $parameters) && $parameters['street'] != '' ? $parameters['street'] : null;
 
-                $search = $service_search->etablissements($label, $identifiant, $genres, $categories, $classes, $familles, $types, $avis_favorable, $statuts, $local_sommeil, null, null, null, $city, $street, 50, $parameters['page']);
+                $search = $service_search->etablissements($label, $identifiant, $genres, $categories, $classes, $familles, $types_activites, $avis_favorable, $statuts, $local_sommeil, null, null, null, $city, $street, 50, $parameters['page']);
 
                 $paginator = new Zend_Paginator(new SDIS62_Paginator_Adapter_Array($search['results'], $search['search_metadata']['count']));
                 $paginator->setItemCountPerPage(50)->setCurrentPageNumber($parameters['page'])->setDefaultScrollingStyle('Elastic');
