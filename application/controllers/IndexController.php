a18994f085a362c947bea1d825f025dfa68ca70d
Kévin DUBUC <kevindubuc62@gmail.com>
2014-11-26 17:30:53 +0100

    rollout : composant ACL dev passé en prod

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -50,8 +50,8 @@ class IndexController extends Zend_Controller_Action
                 'type'    => 'etablissements',
                 'height'  => 'small',
                 'width'   => 'small',
-            ),
-            'ERPOuvertsSousAvisDefavorableSuivis' => array(
+                ),
+                'ERPOuvertsSousAvisDefavorableSuivis' => array(
                 'service' => $service_dashboard,
                 'method'  => 'getERPOuvertsSousAvisDefavorableSuivis',
                 'acl'     => array('dashboard', 'view_ets_avis_defavorable_suivis'),
@@ -59,8 +59,8 @@ class IndexController extends Zend_Controller_Action
                 'type'    => 'etablissements',
                 'height'  => 'small',
                 'width'   => 'small',
-            ),
-            'ERPOuvertsSousAvisDefavorableSurCommune' => array(
+                ),
+                'ERPOuvertsSousAvisDefavorableSurCommune' => array(
                 'service' => $service_dashboard,
                 'method'  => 'getERPOuvertsSousAvisDefavorableSurCommune',
                 'acl'     => array('dashboard', 'view_ets_avis_defavorable_sur_commune'),
@@ -68,8 +68,8 @@ class IndexController extends Zend_Controller_Action
                 'type'    => 'etablissements',
                 'height'  => 'small',
                 'width'   => 'small',
-            ),
-            'ERPSansPreventionniste' => array(
+                ),
+                'ERPSansPreventionniste' => array(
                 'service' => $service_dashboard,
                 'method'  => 'getERPSansPreventionniste',
                 'acl'     => array('dashboard', 'view_ets_sans_preventionniste'),
@@ -77,8 +77,8 @@ class IndexController extends Zend_Controller_Action
                 'type'    => 'etablissements',
                 'height'  => 'small',
                 'width'   => 'small',
-            ),
-            'ERPOuvertsSansProchainesVisitePeriodiques' => array(
+                ),
+                'ERPOuvertsSansProchainesVisitePeriodiques' => array(
                 'service' => $service_dashboard,
                 'method'  => 'getERPOuvertsSansProchainesVisitePeriodiques',
                 'acl'     => array('dashboard', 'view_ets_ouverts_sans_prochaine_vp'),
@@ -86,10 +86,10 @@ class IndexController extends Zend_Controller_Action
                 'type'    => 'etablissements',
                 'height'  => 'small',
                 'width'   => 'small',
-            ),
+                ),
 
-            // lié aux dossiers
-            'DossiersSuivisSansAvis' => array(
+                // lié aux dossiers
+                'DossiersSuivisSansAvis' => array(
                 'service' => $service_dashboard,
                 'method'  => 'getDossiersSuivisSansAvis',
                 'acl'     => array('dashboard', 'view_doss_suivis_sans_avis'),
@@ -97,8 +97,8 @@ class IndexController extends Zend_Controller_Action
                 'type'    => 'dossiers',
                 'height'  => 'small',
                 'width'   => 'small',
-            ),
-            'DossiersSuivisNonVerrouilles' => array(
+                ),
+                'DossiersSuivisNonVerrouilles' => array(
                 'service' => $service_dashboard,
                 'method'  => 'getDossiersSuivisNonVerrouilles',
                 'acl'     => array('dashboard', 'view_doss_suivis_unlocked'),
@@ -106,8 +106,8 @@ class IndexController extends Zend_Controller_Action
                 'type'    => 'dossiers',
                 'height'  => 'small',
                 'width'   => 'small',
-            ),
-            'DossierDateCommissionEchu' => array(
+                ),
+                'DossierDateCommissionEchu' => array(
                 'service' => $service_dashboard,
                 'method'  => 'getDossierDateCommissionEchu',
                 'acl'     => array('dashboard', 'view_doss_sans_avis'),
@@ -115,8 +115,8 @@ class IndexController extends Zend_Controller_Action
                 'type'    => 'dossiers',
                 'height'  => 'small',
                 'width'   => 'small',
-            ),
-            'DossierAvecAvisDiffere' => array(
+                ),
+                'DossierAvecAvisDiffere' => array(
                 'service' => $service_dashboard,
                 'method'  => 'getDossierAvecAvisDiffere',
                 'acl'     => array('dashboard', 'view_doss_avis_differe'),
@@ -124,8 +124,8 @@ class IndexController extends Zend_Controller_Action
                 'type'    => 'dossiers',
                 'height'  => 'small',
                 'width'   => 'small',
-            ),
-            'CourrierSansReponse' => array(
+                ),
+                'CourrierSansReponse' => array(
                 'service' => $service_dashboard,
                 'method'  => 'getCourrierSansReponse',
                 'acl'     => array('dashboard', 'view_courrier_sans_reponse'),
@@ -133,10 +133,10 @@ class IndexController extends Zend_Controller_Action
                 'type'    => 'dossiers',
                 'height'  => 'small',
                 'width'   => 'small',
-            ),
+                ),
 
-            // autres blocs
-            'feeds' => array(
+                // autres blocs
+                'feeds' => array(
                 'service' => $service_feed,
                 'method'  => 'getFeeds',
                 'acl'     => null,
@@ -144,78 +144,80 @@ class IndexController extends Zend_Controller_Action
                 'type'    => 'feeds',
                 'height'  => 'small',
                 'width'   => 'small',
-            ),
+                ),
 
-        );
-
-        $identity = Zend_Auth::getInstance()->getIdentity();
-        $user = $service_user->find($identity['ID_UTILISATEUR']);
-        $acl = Zend_Controller_Front::getInstance()->getParam('bootstrap')->getResource('acl');
-        $profil = $user['group']['LIBELLE_GROUPE'];
-        $blocs = array();
-        foreach($blocsConfig as $blocId => $blocConfig) {
-            if (!$blocConfig['acl'] || ($acl->isAllowed($profil, $blocConfig['acl'][0], $blocConfig['acl'][1]))) {
-                $method = $blocConfig['method'];
-                $blocs[$blocId] = array(
-                    'data' => $blocConfig['service']->$method($user),
-                    'type' => $blocConfig['type'],
-                    'title' => $blocConfig['title'],
-                    'height' => $blocConfig['height'],
-                    'width' => $blocConfig['width'],
                 );
-            }
-        }
 
-        // determine the bloc order
-        // user preferences
-        if (isset($user['preferences']['DASHBOARD_BLOCS'])
+                $identity = Zend_Auth::getInstance()->getIdentity();
+                $user = $service_user->find($identity['ID_UTILISATEUR']);
+                $cache = Zend_Controller_Front::getInstance()->getParam('bootstrap')->getResource('cache');
+                $acl = unserialize($cache->load('acl'));
+                $profil = $user['group']['LIBELLE_GROUPE'];
+                $blocs = array();
+                foreach($blocsConfig as $blocId => $blocConfig) {
+                    if (!$blocConfig['acl'] || ($acl->isAllowed($profil, $blocConfig['acl'][0], $blocConfig['acl'][1]))) {
+                        $method = $blocConfig['method'];
+                        $blocs[$blocId] = array(
+                        'data' => $blocConfig['service']->$method($user),
+                        'type' => $blocConfig['type'],
+                        'title' => $blocConfig['title'],
+                        'height' => $blocConfig['height'],
+                        'width' => $blocConfig['width'],
+                        );
+                    }
+                }
+
+                // determine the bloc order
+                // user preferences
+                if (isset($user['preferences']['DASHBOARD_BLOCS'])
                 && $user['preferences']['DASHBOARD_BLOCS']
                 && $blocsOrder = json_decode($user['preferences']['DASHBOARD_BLOCS'])
-        ) {
-            // treat the case where there will be new bloc added
-            foreach(array_keys($blocsConfig) as $defaultBloc) {
-                if (!in_array($defaultBloc, $blocsOrder)) {
-                    $blocsOrder[] = $defaultBloc;
+                ) {
+                    // treat the case where there will be new bloc added
+                    foreach(array_keys($blocsConfig) as $defaultBloc) {
+                        if (!in_array($defaultBloc, $blocsOrder)) {
+                            $blocsOrder[] = $defaultBloc;
+                        }
+                    }
+                } else {
+                    $blocsOrder = array_keys($blocsConfig);
+                }
+
+                $this->view->user = $user;
+                $this->view->blocs = $blocs;
+                $this->view->blocsOrder = $blocsOrder;
+                $this->view->inlineScript()->appendFile("/js/jquery.packery.pkgd.min.js");
+                $this->_helper->layout->setLayout('index');
+                $this->render('index');
+            }
+
+            public function addMessageAction()
+            {
+                $service_feed = new Service_Feed;
+                $service_user = new Service_User;
+
+                $this->view->groupes = $service_user->getAllGroupes();
+                if ($this->_request->isPost()) {
+                    try {
+                        $service_feed->addMessage($this->_request->getParam('type'), $this->_request->getParam('text'), Zend_Auth::getInstance()->getIdentity()['ID_UTILISATEUR'], $this->_request->getParam('conf') );
+                        $this->_helper->flashMessenger(array('context' => 'success','title' => 'Message ajouté !','message' => 'Le message a bien été ajouté.'));
+                    } catch (Exception $e) {
+                        $this->_helper->flashMessenger(array('context' => 'danger','title' => 'Erreur !','message' => 'Erreur lors de l\'ajout du message : ' . $e->getMessage()));
+                    }
+                    $this->_helper->redirector('index', 'index');
                 }
             }
-        } else {
-            $blocsOrder = array_keys($blocsConfig);
-        }
 
-        $this->view->user = $user;
-        $this->view->blocs = $blocs;
-        $this->view->blocsOrder = $blocsOrder;
-        $this->view->inlineScript()->appendFile("/js/jquery.packery.pkgd.min.js");
-        $this->_helper->layout->setLayout('index');
-    }
+            public function deleteMessageAction()
+            {
+                $service_feed = new Service_Feed;
 
-    public function addMessageAction()
-    {
-        $service_feed = new Service_Feed;
-        $service_user = new Service_User;
-
-        $this->view->groupes = $service_user->getAllGroupes();
-        if ($this->_request->isPost()) {
-            try {
-                $service_feed->addMessage($this->_request->getParam('type'), $this->_request->getParam('text'), Zend_Auth::getInstance()->getIdentity()['ID_UTILISATEUR'], $this->_request->getParam('conf') );
-                $this->_helper->flashMessenger(array('context' => 'success','title' => 'Message ajouté !','message' => 'Le message a bien été ajouté.'));
-            } catch (Exception $e) {
-                $this->_helper->flashMessenger(array('context' => 'danger','title' => 'Erreur !','message' => 'Erreur lors de l\'ajout du message : ' . $e->getMessage()));
+                try {
+                    $service_feed->deleteMessage($this->_request->getParam('id'));
+                    $this->_helper->flashMessenger(array('context' => 'success','title' => 'Message supprimé !','message' => 'Le message a bien été supprimé.'));
+                } catch (Exception $e) {
+                    $this->_helper->flashMessenger(array('context' => 'danger','title' => 'Erreur !','message' => 'Erreur lors de la suppression du message : ' . $e->getMessage()));
+                }
+                $this->_helper->redirector('index', 'index');
             }
-            $this->_helper->redirector('index', 'index');
         }
-    }
-
-    public function deleteMessageAction()
-    {
-        $service_feed = new Service_Feed;
-
-        try {
-            $service_feed->deleteMessage($this->_request->getParam('id'));
-            $this->_helper->flashMessenger(array('context' => 'success','title' => 'Message supprimé !','message' => 'Le message a bien été supprimé.'));
-        } catch (Exception $e) {
-            $this->_helper->flashMessenger(array('context' => 'danger','title' => 'Erreur !','message' => 'Erreur lors de la suppression du message : ' . $e->getMessage()));
-        }
-        $this->_helper->redirector('index', 'index');
-    }
-}
