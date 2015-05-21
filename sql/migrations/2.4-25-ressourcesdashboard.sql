769d5b1e4e07d181db16edcaa35f9a85d2df39a6
Kévin DUBUC <kevindubuc62@gmail.com>
2015-05-21 10:05:55 +0200

    On évite le troncage sur le champs desc

--- sql/migrations/2.4-25-ressourcesdashboard.sql
+++ sql/migrations/2.4-25-ressourcesdashboard.sql
@@ -1,3 +1,3 @@
 SET NAMES 'utf8';
 
-INSERT INTO `privileges`(`id_privilege`,`name`, `text`,`id_resource`) VALUES(NULL,"view_doss_levee_prescriptions", "Voir les dossiers avec une date de levée des prescriptions",100);
+INSERT INTO `privileges`(`id_privilege`,`name`, `text`,`id_resource`) VALUES(NULL,"view_doss_levee_prescriptions", "Date de levée des prescriptions",100);
