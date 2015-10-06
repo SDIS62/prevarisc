20e23c3b50329431c160085f598caf4fe3894044
A511701 <vincent.michel@atos.net>
2015-10-06 12:30:01 +0200

    Ajout des modifications de structures de base a effectuer

---
+++ sql/migrations/2.4-28_cache.sql
@@ -0,0 +1,21 @@
+SET NAMES 'utf8';
+
+/* Creates caching table structure in business database */
+CREATE TABLE `cache` (
+  `ID_CACHE` varchar(250) NOT NULL,
+  `VALUE_CACHE` text,
+  `EXPIRE_CACHE` int,
+  PRIMARY KEY (`ID_CACHE`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/* Enables mysql memcache plugin */
+/* Path to mysql cache plugin depends on your installation */
+SOURCE '/usr/share/mysql/innodb_memcached_config.sql';
+INSTALL PLUGIN daemon_memcached SONAME 'libmemcached.so';
+
+/* Declares container configuration and clean previous one*/
+USE innodb_memcache;
+
+DELETE FROM containers;
+INSERT INTO `containers` (`name`, `db_schema`, `db_table`, `key_columns`, `value_columns`, `flags`, `cas_column`, `expire_time_column`, `unique_idx_name_on_key`)
+VALUES ('prevarisc_cache', 'prevarisc', 'cache', 'ID_CACHE', 'VALUE_CACHE', 0, 0, 'EXPIRE_CACHE', 'PRIMARY');
