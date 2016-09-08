fc6946a38d4b6bba6d823d3bc9fdbff54a56311c
A511701 <vincent.michel@atos.net>
2016-09-08 17:12:25 +0200

    Ajout de la table changement dans le SQL d'init

--- sql/init/prevarisc.sql
+++ sql/init/prevarisc.sql
@@ -2903,3 +2903,23 @@ CREATE TABLE `cache` (
   `EXPIRE_CACHE` int,
   PRIMARY KEY (`ID_CACHE`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+-- -----------------------------------------------------
+-- Table`changement` 
+-- -----------------------------------------------------
+
+DROP TABLE IF EXISTS `changement`;
+
+CREATE TABLE `changement` (
+  `ID_CHANGEMENT` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `LIBELLE_CHANGEMENT` varchar(255) DEFAULT NULL,
+  `MESSAGE_CHANGEMENT` text,
+  PRIMARY KEY (`ID_CHANGEMENT`)
+) 
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `changement` WRITE;
+INSERT INTO `changement` VALUES (1,'Changement de statut','<p>Bonjour,</p>\r\n<p>L\'&eacute;tablissement {etablissementNumeroId} {etablissementLibelle} est pass&eacute; au statut {etablissementStatut}.</p>\r\n<p>Bonne journ&eacute;e,</p>\r\n<p>Pr&eacute;varisc.</p>'),(2,'Changement d\'avis','<p>Bonjour,</p>\r\n<p>L\'&eacute;tablissement {etablissementNumeroId} {etablissementLibelle} est maintenant sous avis {etablissementAvis}.</p>\r\n<p>Bonne journ&eacute;e,</p>\r\n<p>Pr&eacute;varisc.</p>'),(3,'Changement de classement','<p>Bonjour,</p>\r\n<p>L\'&eacute;tablissement {etablissementNumeroId} {etablissementLibelle} est maintenant de cat&eacute;gorie {categorieEtablissement}, de type {typePrincipalEtablissement} - {activitePrincipaleEtablissement}.</p>\r\n<p>Bonne journ&eacute;e,</p>\r\n<p>Pr&eacute;varisc.</p>');
+UNLOCK TABLES;
