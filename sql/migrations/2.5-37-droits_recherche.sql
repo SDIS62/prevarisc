81d3eb9df87b471c789c2867163aa1cfc7e707a1
A511701 <vincent.michel@atos.net>
2016-10-15 14:14:16 +0200

    Correction du SQL d'ajout du droit pour que les utilisateurs existants ne se retrouvent pas bloqués

--- sql/migrations/2.5-37-droits_recherche.sql
+++ sql/migrations/2.5-37-droits_recherche.sql
@@ -6,3 +6,5 @@ INSERT INTO `privileges`(`id_privilege`,`name`, `text`,`id_resource`) VALUES
 (63,'search_ets','Etablissements',57),
 (64,'search_dossiers','Dossiers',57);
 
+INSERT INTO `groupe-privileges`(ID_GROUPE, id_privilege) select id_groupe,63 from groupe;
+INSERT INTO `groupe-privileges`(ID_GROUPE, id_privilege) select id_groupe,64 from groupe;
