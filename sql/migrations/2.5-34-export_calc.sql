335882d91e78a858588a31670fe18bc07655bd0f
cfx <cfx@cfx-ESPRIMO-D556>
2016-12-07 10:46:07 +0100

    Ajout fonctionnalité d'export Calc de la liste des établissements : - Utilisation de la librairie PHPOffice/PHPExcel - Fonctionnalité régie par un nouveau droit (ajout ressource "Export Calc" et privilège "Etablissements" dans la gestion des droits des groupes) - Si l'utilisateur connecté fait partie d'un groupe associé à ce droit, un bouton "Export Calc" est visible sur l'écran de recherche des établissements - Ajout des filtres "Commission" et "Groupement territorial" sur cet écran - A l'arrivée sur cet écran, si l'utilisateur est associé à un groupement territorial (type=5), la liste des établissements est automatiquement filtrée sur ce groupement - A chaque modification d'un filtre de recherche, on doit relancer la recherche pour que le bouton "Export Calc" apparaisse (l'export correspond donc aux filtres et à la liste affichés à l'écran) - Penser à modifier le fichier php.ini pour des problématiques de mémoire cache (augmenter les directives "max_execution_time" et "memory_limit")
    Change-Id: Ib12da9d9d9c3a310920c2efbbefb4cd430ff18a7

---
+++ sql/migrations/2.5-34-export_calc.sql
@@ -0,0 +1,4 @@
+SET NAMES 'utf8';
+
+INSERT INTO `resources`(`id_resource`,`name`, `text`) VALUES(200,"export", "Export Calc");
+INSERT INTO `privileges`(`id_privilege`,`name`, `text`,`id_resource`) VALUES(200,"export_ets", "Etablissements",200);
