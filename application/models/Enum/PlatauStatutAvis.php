0dbca175b8288f399c8ab8a7cbad7def4e431296
Maxime Merrien <maxime.merrien@atos.net>
2023-03-13 11:08:49 +0100

    Ajout d'un statut d'erreur

--- application/models/Enum/PlatauStatutAvis.php
+++ application/models/Enum/PlatauStatutAvis.php
@@ -7,12 +7,14 @@ class Model_Enum_PlatauStatutAvis
         self::EN_COURS => 'En cours',
         self::TRAITE => 'Traité',
         self::A_RENVOYER => 'Traité (en attente de renvoi)',
+        self::EN_ERREUR => 'En erreur',
     ];
 
     public const INCONNU = 'unknown';
     public const EN_COURS = 'in_progress';
     public const TRAITE = 'treated';
     public const A_RENVOYER = 'to_export';
+    public const EN_ERREUR = 'in_error';
 
     public function getLabel(string $enumValue): string
     {
--- application/models/Enum/PlatauStatutPec.php
+++ application/models/Enum/PlatauStatutPec.php
@@ -7,12 +7,14 @@ class Model_Enum_PlatauStatutPec
         self::EN_ATTENTE => 'En attente',
         self::PRISE_EN_COMPTE => 'Prise en compte',
         self::A_RENVOYER => 'Prise en compte (en attente de renvoi)',
+        self::EN_ERREUR => 'En erreur',
     ];
 
     public const INCONNU = 'unknown';
     public const EN_ATTENTE = 'awaiting';
     public const PRISE_EN_COMPTE = 'taken_into_account';
     public const A_RENVOYER = 'to_export';
+    public const EN_ERREUR = 'in_error';
 
     public function getLabel(string $enumValue): string
     {
